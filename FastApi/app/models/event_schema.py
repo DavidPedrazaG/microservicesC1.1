b548b8741bbd92fd73f370143233a40df9cf0746
David Pedraza <davidpipepedraza@gmail.com>
2024-09-07 00:44:41 -0500

    Main

--- FastApi/app/models/event_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Event(BaseModel):
-    id : str
-    name : str 
-    address : str
-    city : str
-    description : str
-    
--- FastApi/app/models/location_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Location(BaseModel):
-    id : str 
-    name : str 
-    price : float
-    max_capacity : int
-    available_seats : str
-    
